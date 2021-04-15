1129bd0562334fbd62705e04719fce0ef277527b
chook <dylanw212@gmail.com>
2021-04-16 09:02:37 +1000

    add basic / testing css for menu layout

--- src/js/contact.js
+++ src/js/contact.js
@@ -1,8 +1,17 @@
+// function createContact() {
+//   const container = document.createElement('div')
+
+//   const textContent = document.createTextNode(contactDetails)
+//   container.append(textContent)
+
+//   return container;
+// }
+
 function createContact() {
   const container = document.createElement('div')
-
-  const textContent = document.createTextNode(contactDetails)
-  container.append(textContent)
+  container.classList.add('menu-container')
+  container.innerHTML =
+  `<div class="product"><div class="img-ph"></div><h3>Product</h3></div><div class="product"><div class="img-ph"></div><h3>Product</h3></div><div class="product"><div class="img-ph"></div><h3>Product</h3></div><div class="product"><div class="img-ph"></div><h3>Product</h3></div><div class="product"><div class="img-ph"></div><h3>Product</h3></div><div class="product"><div class="img-ph"></div><h3>Product</h3></div><div class="product"><div class="img-ph"></div><h3>Product</h3></div><div class="product"><div class="img-ph"></div><h3>Product</h3></div><div class="product"><div class="img-ph"></div><h3>Product</h3></div><div class="product"><div class="img-ph"></div><h3>Product</h3></div><div class="product"><div class="img-ph"></div><h3>Product</h3></div>`
 
   return container;
 }
@@ -11,3 +20,8 @@ export { createContact }
 
 
 const contactDetails = 'You can call me on my telephone. Or you can call me Mr Telephone.'
+
+
+// Testing purposes only
+
+const testHTML = `<div class="menu-container><div class="product"><div class="img-ph><h3>Product</h3></div></div></div>`
