3e9e8129d05067533cb49604fcf0416e344c2680
chook <dylanw212@gmail.com>
2021-04-15 12:12:54 +1000

    img now being created dynamically

--- src/js/landing.js
+++ src/js/landing.js
@@ -1,8 +1,12 @@
 function createLanding() {
   const container = document.createElement('div');
   container.classList.add('landing');
-  const image = document.createElement('div')
-  image.textContent = 'Img goes here';
+
+  const imageContainer = document.createElement('div')
+  const img = document.createElement('img')
+  img.src = '../src/assets/burger-img.jpg'
+  imageContainer.append(img);
+
   const info = document.createElement('div');
   info.classList.add('landing-info')
   const heading = document.createElement('h2')
@@ -11,7 +15,8 @@ function createLanding() {
   const infoText = document.createElement('div');
   infoText.textContent = 'Borger Borger Borger Borger Borger Borger Borger Borger Borger Borger Borger Borger Borger Borger Borger Borger Borger Borger'
   info.append(infoText)
-  container.append(image);
+
+  container.append(imageContainer);
   container.append(info);
 
   return container;
