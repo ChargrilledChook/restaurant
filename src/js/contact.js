4b3e6deaf8c5f302aefdeffddb41b278b5464fab
chook <dylanw212@gmail.com>
2021-04-16 12:37:56 +1000

    remove testing cruft from contact and put placeholder

--- src/js/contact.js
+++ src/js/contact.js
@@ -1,17 +1,8 @@
-// function createContact() {
-//   const container = document.createElement('div')
-
-//   const textContent = document.createTextNode(contactDetails)
-//   container.append(textContent)
-
-//   return container;
-// }
 
 function createContact() {
   const container = document.createElement('div')
-  container.classList.add('menu-container')
-  container.innerHTML =
-  `<div class="product"><div class="img-ph"></div><h3>Product</h3></div><div class="product"><div class="img-ph"></div><h3>Product</h3></div><div class="product"><div class="img-ph"></div><h3>Product</h3></div><div class="product"><div class="img-ph"></div><h3>Product</h3></div><div class="product"><div class="img-ph"></div><h3>Product</h3></div><div class="product"><div class="img-ph"></div><h3>Product</h3></div><div class="product"><div class="img-ph"></div><h3>Product</h3></div><div class="product"><div class="img-ph"></div><h3>Product</h3></div><div class="product"><div class="img-ph"></div><h3>Product</h3></div><div class="product"><div class="img-ph"></div><h3>Product</h3></div><div class="product"><div class="img-ph"></div><h3>Product</h3></div>`
+  container.classList.add('contact-container')
+  container.textContent = contactDetails;
 
   return container;
 }
@@ -20,8 +11,3 @@ export { createContact }
 
 
 const contactDetails = 'You can call me on my telephone. Or you can call me Mr Telephone.'
-
-
-// Testing purposes only
-
-const testHTML = `<div class="menu-container><div class="product"><div class="img-ph><h3>Product</h3></div></div></div>`
