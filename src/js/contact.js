90759f516ae98fa3c3dd2eafddd85fa24fafb903
chook <dylanw212@gmail.com>
2021-04-19 11:11:37 +1000

    add basic layout for contact page

--- src/js/contact.js
+++ src/js/contact.js
@@ -2,12 +2,39 @@
 function createContact() {
   const container = document.createElement('div')
   container.classList.add('contact-container')
-  container.textContent = contactDetails;
+
+  const heading = document.createElement('h2');
+  heading.textContent = 'Contact Us';
+
+  const phone = createBasicElement(phoneTitle, phoneContent);
+  const email = createBasicElement(mailTitle, mailContent);
+  const address = createBasicElement(addTitle, addContent);
+
+  container.append(heading, phone, email, address);
 
   return container;
 }
 
+function createBasicElement(title, content){
+  const container = document.createElement('div');
+  const titleElt = document.createElement('h3');
+  const contentElt = document.createElement('div');
+
+  titleElt.textContent = title;
+  contentElt.textContent = content;
+
+  container.append(titleElt, contentElt);
+
+  return container;
+}
+
+const phoneTitle = 'Phone';
+const phoneContent = ' 0267 XXX XXX'
+
+const mailTitle = 'E-mail';
+const mailContent = 'eatBORGER@borgers.com';
+
+const addTitle = 'Address';
+const addContent = '123 BORGER Way BORGER Town';
+
 export { createContact }
-
-
-const contactDetails = 'You can call me on my telephone. Or you can call me Mr Telephone.'
