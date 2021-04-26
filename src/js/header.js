998e8c543009d6f458e513acf69d15265d4c22aa
chook <dylanw212@gmail.com>
2021-04-26 13:34:27 +1000

    add media queries to header

--- src/js/header.js
+++ src/js/header.js
@@ -1,31 +1,34 @@
-export function createHeader(){
-  const container = document.createElement('header');
-  container.classList.add('site-header');
+export function createHeader() {
+  const container = document.createElement("header");
+  container.classList.add("site-header");
 
-  const heading = document.createElement('div');
+  const heading = document.createElement("div");
   heading.textContent = restaurantTitle;
 
-  const nav = document.createElement('nav');
+  const nav = document.createElement("nav");
 
-  const home = document.createElement('a');
-  home.href = '#';
-  home.textContent = 'Home';
-  home.id = 'home';
+  const navIcon = document.createElement("img");
+  navIcon.src = "./assets/menu_icon.svg";
 
-  const menu = document.createElement('a');
-  menu.href = '#';
-  menu.textContent = 'Menu';
-  menu.id = 'menu';
+  const home = document.createElement("a");
+  home.href = "#";
+  home.textContent = "Home";
+  home.id = "home";
 
-  const contact = document.createElement('a');
-  contact.href = '#';
-  contact.textContent = 'Contact';
-  contact.id = 'contact';
+  const menu = document.createElement("a");
+  menu.href = "#";
+  menu.textContent = "Menu";
+  menu.id = "menu";
 
-  nav.append(home, menu, contact);
+  const contact = document.createElement("a");
+  contact.href = "#";
+  contact.textContent = "Contact";
+  contact.id = "contact";
+
+  nav.append(navIcon, home, menu, contact);
   container.append(heading, nav);
 
   return container;
 }
 
-const restaurantTitle = 'Eat Borger';
+const restaurantTitle = "Eat Borger";
