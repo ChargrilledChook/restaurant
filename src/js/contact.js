b63ec7757f94cbdf235b3721c1ebeec5de99c58e
chook <dylanw212@gmail.com>
2021-04-28 07:20:23 +1000

    change export on data for consistency and to reveal intentions

--- src/js/contact.js
+++ src/js/contact.js
@@ -9,31 +9,31 @@ export class Contact {
 
   get page() {
     return this._createContact();
-  };
+  }
 
   _createContact() {
-    const container = document.createElement('div')
-    container.classList.add('contact-container')
+    const container = document.createElement("div");
+    container.classList.add("contact-container");
 
-    const heading = document.createElement('h2');
-    heading.textContent = 'Contact Us';
+    const heading = document.createElement("h2");
+    heading.textContent = "Contact Us";
 
-    const phone = this._createBasicElement('Phone', this.phone);
-    const email = this._createBasicElement('E-mail', this.mail);
-    const address = this._createBasicElement('Address', this.address);
-    const hours = this._createBasicElement('Opening Hours', this.hours);
+    const phone = this._createBasicElement("Phone", this.phone);
+    const email = this._createBasicElement("E-mail", this.mail);
+    const address = this._createBasicElement("Address", this.address);
+    const hours = this._createBasicElement("Opening Hours", this.hours);
     const map = this._createMap(this.map);
-    address.append(map)
+    address.append(map);
 
     container.append(heading, phone, email, hours, address);
 
     return container;
   }
 
-  _createBasicElement(title, content){
-    const container = document.createElement('div');
-    const titleElt = document.createElement('h3');
-    const contentElt = document.createElement('div');
+  _createBasicElement(title, content) {
+    const container = document.createElement("div");
+    const titleElt = document.createElement("h3");
+    const contentElt = document.createElement("div");
 
     titleElt.textContent = title;
     contentElt.textContent = content;
@@ -44,13 +44,12 @@ export class Contact {
   }
 
   _createMap() {
-    const container = document.createElement('iframe')
+    const container = document.createElement("iframe");
     container.src = this.map;
-    container.width = '600';
-    container.height = '450';
-    // container.loading = 'lazy';
-    container.style.border = '0';
+    container.width = "600";
+    container.height = "450";
+    container.style.border = "0";
 
     return container;
   }
-};
+}
--- src/js/data.js
+++ src/js/data.js
@@ -9,12 +9,12 @@ const imgPath = "./assets/";
 
 // Header =======
 const title = "Eat Burger";
-const header = createHeader(title);
+export const header = createHeader(title);
 
 // Footer =======
 const citation = "Created 2021 by Dylan. View my Code on";
 const link = "https://github.com/ChargrilledChook/restaurant";
-const footer = createFooter(citation, link);
+export const footer = createFooter(citation, link);
 
 // Landing ======
 const landingTitle = "It's burger time!";
@@ -24,7 +24,7 @@ const landingCopy = [
   "Eat burger is a retro-futuristic yet modern take on traditional burger based fine dining. Come in and enjoy our hand selected, de-constructed and re-constructed locally havested sumptuous compositions. Our thrice activated twice distilled beef patties will have your mouth watering and your tastebuds dancing.",
   "Call now or order online and begin your ascent to burger nirvana. Any time is a good time to Eat Burger. Kids eat free!",
 ];
-const landing = new Landing(landingTitle, landingImg, landingCopy);
+export const landing = new Landing(landingTitle, landingImg, landingCopy);
 
 // Menu =========
 const rawProductData = [
@@ -39,7 +39,7 @@ const rawProductData = [
   ["Three's a Crowd Burger", "burg9.png"],
   ["Pile on the Poulet Burger", "burg10.png"],
 ];
-const menu = new Menu(rawProductData, imgPath);
+export const menu = new Menu(rawProductData, imgPath);
 
 // Contact ======
 
@@ -49,6 +49,4 @@ const hours = "9 - 5 on the 32nd of every month";
 const address = "123 Burger Way Borger Town";
 const map =
   "https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d59000.70533535844!2d9.285668941264133!3d56.663472316254534!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sborger!5e0!3m2!1sen!2sau!4v1618795242001!5m2!1sen!2sau";
-const contact = new Contact(phone, mail, hours, address, map);
-
-export { header, landing, menu, contact, footer };
+export const contact = new Contact(phone, mail, hours, address, map);
